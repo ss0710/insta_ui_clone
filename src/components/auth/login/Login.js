ce16aa4d96403e2ced7a4bc4edafca3ce381eeb2
Sudheer Singh <sudheersid0710@gmail.com>
2020-12-22 19:28:07 +0530

    added images in login.js

--- src/components/auth/login/Login.js
+++ src/components/auth/login/Login.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import Phone from '../../../assets/images/phone.PNG';
+import InstagramLogo from '../../../assets/images/instagramLogo.PNG';
 
 const Login  = () => {
     return(
         <div className="login-div">
             <div className="login-container">
                  <div className="container-one">
-
+                    <img width={380} height={600} src={Phone} alt="login-img"/>
                  </div>
                  <div className="container-two">
-
+                    <div className="form-div">
+                    <img src={InstagramLogo} alt="instagram-logo"/>
+                    </div>
                  </div>
             </div>
         </div>

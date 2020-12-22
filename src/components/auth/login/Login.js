43c5a3369bfdcab1532a1fc22ab7a67e8b4db78a
Sudheer Singh <sudheersid0710@gmail.com>
2020-12-22 22:49:14 +0530

    updated UI of Login

--- src/components/auth/login/Login.js
+++ src/components/auth/login/Login.js
@@ -8,10 +8,13 @@ const Login  = () => {
     return(
         <div className="login-div">
             <div className="login-container">
+
                  <div className="container-child">
                     <img width={380} height={600} src={Phone} alt="login-img"/>
                  </div>
+
                  <div className="container-child">
+
                     <div className="form-div">
                     <img width={280} height={70} src={InstagramLogo} alt="instagram-logo"/>
                     <InputTextField type={'text'} placeholder={'username'} />
@@ -23,7 +26,18 @@ const Login  = () => {
                     <h5>Forgotten Passsword?</h5>
                     </div>
                     </div>
+
+                    <div className="signup-link-div">
+                    <h4>Don't have an account? Sign up</h4>
+                    </div>
+
+                    <div>
+                    <p>Get the app.</p>
+                    <button>App Store</button><button>Google Play</button>
+                    </div>
+
                  </div>
+
             </div>
         </div>
     );

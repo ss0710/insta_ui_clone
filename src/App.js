c72190d9769a7f4cae395346ddebab7a7553a8f7
Sudheer Singh <sudheersid0710@gmail.com>
2020-12-23 21:49:44 +0530

    completed Navbar

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.scss';
 import {Route , Switch} from 'react-router-dom';
 import Login from './components/auth/login/Login';
 import Signup from './components/auth/signup/Signup';
-import Navbar from './components/navBar/Navbar';
+import Home from './components/homepage/Home';
 
 function App() {
   return (
     <div className="App">
-    {/* <Switch>
+    <Switch>
     <Route exact path='/' component={Login}/>
     <Route path='/Signup' component={Signup}/>
-    </Switch> */}
-    <Navbar />
+    <Route path='/Home' component={Home}/>
+    </Switch>
     </div>
   );
 }
--- src/components/navBar/Navbar.js
+++ src/components/navBar/Navbar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import Logo from '../../assets/images/logo.PNG';
+import HomeIcon from '@material-ui/icons/HomeOutlined';
+import MessengerIcon from '@material-ui/icons/MessageOutlined';
+import ExploreIcon from '@material-ui/icons/ExploreOutlined';
+import LikeIcon from '@material-ui/icons/FavoriteOutlined';
 
 const Navbar = () => {
     return (
         <div className="navbar-container">
             <div className="navbar">
                 <div>
-                    <img width={125} height={45} src={Logo} alt="logo"/>
+                    <img width={135} height={45} src={Logo} alt="logo"/>
                 </div>
                 <div>
-                    searchbar
+                    <input type="text" placeholder="search" />
                 </div>
-                <div>
-                    icon
+                <div className="icons">
+                    <HomeIcon className="icon"/>
+                    <MessengerIcon className="icon"/>
+                    <ExploreIcon className="icon"/>
+                    <LikeIcon className="icon"/>
                 </div>
             </div>
         </div>
